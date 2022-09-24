94d4727ed32b4a9250bf78398a1b37a8072b8ac9
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-09-24 17:39:05 +0530

    :fire: Added the basic file structure and Changed the favicon with two genius icon

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
-
+import Navbar from "./Components/Navbar";
+import Projects from "./Components/Projects"
 function App() {
-  return <div className="App">Hello World</div>;
+  return <div className="App">
+    <Navbar />
+    <Projects/>
+  </div>;
 }
 
 export default App;
