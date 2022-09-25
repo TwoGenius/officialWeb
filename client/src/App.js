8926da813b0afdf0b03cb49adf00d16d0b45e07c
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-09-25 14:27:38 +0530

    :fire: created the advantages component -> completed its html , css left

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Projects from "./Components/Projects"
+import Advantages from "./Components/Advantages"
 function App() {
   return <div className="App">
     <Navbar />
-    <Projects/>
+    {/* <Projects/> */}
+    <Advantages />
   </div>;
 }
 
---
+++ client/src/Components/Advantages.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./advantages.css"
+import Adv1 from "../Assets/Adantage-2.svg";
+import Adv2 from "../Assets/Advantage-1.svg";
+import Ellipse from "../Assets/Advantages-Circle.svg";
+const Advantages = () => {
+  return (
+    <div className='Advantages'>
+      <div className="left-div">
+        <img src={Adv1} alt="" />
+        <img src={Adv2} alt="" />
+      </div>
+      <div className="right-div">
+       <h2>The Advantages of Our VR That We Offer So Far</h2>
+       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus nostrum aut porro.</p>
+       <button>Get It Now</button>
+      </div>
+      <img src={Ellipse} alt="" />
+    </div>
+  )
+}
+
+export default Advantages
