840b1c5cd341ce284f788df5ee90086cecec2381
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-09-26 17:57:45 +0530

    :fire: Project done

--- client/src/Components/Advantages.jsx
+++ client/src/Components/Advantages.jsx
@@ -7,15 +7,15 @@ const Advantages = () => {
   return (
     <div className='Advantages'>
       <div className="left-div">
-        <img src={Adv1} alt="" />
-        <img src={Adv2} alt="" />
+        <img src={Adv1} alt=""  className="img-1" />
+        <img src={Adv2} alt="" className='img-2' />
       </div>
       <div className="right-div">
        <h2>The Advantages of <br /> Our VR That We <br /> Offer So Far</h2>
        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus nostrum aut porro.</p>
        <button>Get It Now</button>
       </div>
-      <img src={Ellipse} alt="" />
+      <img src={Ellipse} alt="" className="Ellipse" />
     </div>
   )
 }
--- client/src/Components/Projects.jsx
+++ client/src/Components/Projects.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
+import "./project.css"
 import Project1 from '../assets/hackathon.jpg'
 import Project2 from '../assets/learn-to-code.jpg'
 const Projects = () => {
   return (
     <div className="Projects">
       <div className="Heading_and_button">
-        <h2>Different Projects We Built </h2>
-        <h2>on Different Domains</h2>
-        <button>See All</button>
+        <h2>Different Projects We Built <br /> on Different Domains</h2>
+        <button className='SeeAllBtn' >See All</button>
       </div>
       <div className="Cards">
+         <div className="row one">
          <img src={Project1} alt="" />
          <img src={Project2} alt="" />
-         <img src={Project2} alt="" />
+         </div>
+        <div className="row two">
+        <img src={Project2} alt="" />
          <img src={Project1} alt="" />
-      </div>
+        </div>
+      </div> 
     </div>
   )
 }
